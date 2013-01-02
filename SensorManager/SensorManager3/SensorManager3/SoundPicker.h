419fa3c4017107cc48c0fa888f552a77bd4e9422
Set <set.minami@gmail.com>
2013-01-02 16:52:50 +0900

    commit

--- SensorManager/SensorManager3/SensorManager3/SoundPicker.h
+++ SensorManager/SensorManager3/SensorManager3/SoundPicker.h
@@ -8,19 +8,46 @@
 
 #import <UIKit/UIKit.h>
 #import <AudioToolbox/AudioToolbox.h>
+#import <AudioToolbox/AudioFile.h>
 #import "Set_ViewController.h"
 #import "AbstractSensors.h"
 
+#define NUM_OF_BUFFERS 3
+#define SOUND_PACKS 20
 @interface SoundPicker : AbstractSensors{
-     AudioQueueRef queue;
+    AudioFileID audioFile;
+    AudioStreamBasicDescription dataFormat;
+    AudioQueueRef queue;
+    AudioQueueBufferRef buffers[NUM_OF_BUFFERS];
+    CFURLRef fileURL;
+    SInt64 currentPacket;
+    BOOL isRecording;
+    
 }
 
 @property float Volume;
 @property float Interval;
+@property float peakMeterBootupTime;
+
+@property AudioQueueRef queue;
+@property AudioFileID audioFile;
+@property SInt64 currentPacket;
+@property BOOL isRecording;
 
 -(BOOL) canBeThread;
 -(void) start;
 -(void) main;
 -(void) stop;
 
+-(void) startRecording;
+-(void) stopRecording;
+
+
+void AudioInputCallback(    void* inUserData,
+                               AudioQueueRef inAQ,
+                               AudioQueueBufferRef inBuffer,
+                               const AudioTimeStamp* inStartTime,
+                               UInt32 inNumberPacketDescriptions,
+                               const AudioStreamPacketDescription* inPacketDescs
+                               );
 @end
--- SensorManager/SensorManager3/SensorManager3/SoundPicker.m
+++ SensorManager/SensorManager3/SensorManager3/SoundPicker.m
@@ -8,20 +8,17 @@
 
 #import "SoundPicker.h"
 
-@interface SoundPicker (){
-
-}
-- (void)start;
-- (void)fire;
-//@property (retain,nonatomic) NSThread* thread;
-@end
 
 @implementation SoundPicker
 
+@synthesize queue,currentPacket,audioFile,isRecording;
+
 -(void) awake{
     NSLog(@"MIC awake!");
-    _Volume = -0.2f;
-    _Interval = 0.3;
+    _Volume = 5.0f;
+    _Interval = 0.5;
+    _peakMeterBootupTime = 1;
+    
     [self start];
 }
 
@@ -35,54 +32,54 @@
 }
 
 
-static void AudioInputCallback(
-                               void* inUserData,
-                               AudioQueueRef inAQ,
-                               AudioQueueBufferRef inBuffer,
-                               const AudioTimeStamp *inStartTime,
-                               UInt32 inNumberPacketDescriptions,
-                               const AudioStreamPacketDescription *inPacketDescs) {
-}
 
 - (void)start {
-    AudioStreamBasicDescription dataFormat;
+    isRecording = NO;
     dataFormat.mSampleRate = 44100.0f;
-    dataFormat.mFormatID = kAudioFormatLinearPCM;
-    dataFormat.mFormatFlags = kLinearPCMFormatFlagIsBigEndian | kLinearPCMFormatFlagIsSignedInteger | kLinearPCMFormatFlagIsPacked;
+    dataFormat.mFormatID = kAudioFormatLinearPCM;//kAudioFormatMPEG4AAC_HE
+    dataFormat.mFormatFlags = kLinearPCMFormatFlagIsBigEndian|kLinearPCMFormatFlagIsSignedInteger | kLinearPCMFormatFlagIsPacked;
     dataFormat.mBytesPerPacket = 2;
     dataFormat.mFramesPerPacket = 1;
-    dataFormat.mBytesPerFrame = 2;
+    dataFormat.mBytesPerFrame = 2 ;
     dataFormat.mChannelsPerFrame = 1;
     dataFormat.mBitsPerChannel = 16;
+    
     dataFormat.mReserved = 0;
     
-    AudioQueueNewInput(&dataFormat,AudioInputCallback,(__bridge void *)(self),CFRunLoopGetCurrent(),kCFRunLoopCommonModes,0,&queue);
+    AudioQueueNewInput(&dataFormat,AudioInputCallback,(__bridge void *)(self),NULL,kCFRunLoopCommonModes,0,&queue);
     AudioQueueStart(queue, NULL);
     
     UInt32 enabledLevelMeter = true;
-    AudioQueueSetProperty(queue,kAudioQueueProperty_EnableLevelMetering,&enabledLevelMeter,sizeof(UInt32));
-    
+    AudioQueueSetProperty(queue,kAudioQueueProperty_EnableLevelMetering,
+                        &enabledLevelMeter,sizeof(UInt32));
+    BOOL isFirstTime = YES;
     while(1){
-        [self updateVolume];
-        [NSThread sleepForTimeInterval:0.5];
+        [self updateVolume:isFirstTime];
+        if(isFirstTime){[NSThread sleepForTimeInterval:_peakMeterBootupTime];
+        }else{[NSThread sleepForTimeInterval:_Interval];}
+        isFirstTime = NO;
     }
 }
 
-- (void)updateVolume {
+- (void)updateVolume:(BOOL)isFirstTime {
     AudioQueueLevelMeterState levelMeter;
     UInt32 levelMeterSize = sizeof(AudioQueueLevelMeterState);
     AudioQueueGetProperty(queue,kAudioQueueProperty_CurrentLevelMeterDB,&levelMeter,&levelMeterSize);
     
-    //NSLog(@"mPeakPower=%0.9f", levelMeter.mPeakPower);
-    //NSLog(@"mAveragePower=%0.9f", levelMeter.mAveragePower);
+    NSLog(@"mPeakPower=%0.3f", levelMeter.mPeakPower);
+    NSLog(@"mAveragePower=%0.3f", levelMeter.mAveragePower);
+
     
-    if (levelMeter.mPeakPower >= _Volume) {
-        [self fire];
+    if(!isFirstTime&&(levelMeter.mPeakPower > _Volume)) {
+        [self startRecording];
+        [NSThread sleepForTimeInterval:SOUND_PACKS];
+        [self stopRecording];
     }
+
 }
 
-- (void)fire {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"I can hear you..."
+- (void)startRecording {
+/*    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"I can hear you..."
                                                     message:@"Recording!!"
                                                     delegate:nil
                                                     cancelButtonTitle:nil
@@ -90,13 +87,98 @@ static void AudioInputCallback(
     
     [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:YES];
     [alert dismissWithClickedButtonIndex:0 animated:YES];
-    NSLog(@"Recording!");
+  */
+    NSString* filePath = [NSString stringWithFormat:@"%@/hoge.aiff",[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"]];
+    fileURL = CFURLCreateFromFileSystemRepresentation(NULL, (const UInt8*)[filePath UTF8String], [filePath length], NO);
+    NSLog(@"%@%@",@"Start Recording! :",filePath);
+    currentPacket = 0;
+    isRecording = YES;
+    /*
+    AudioQueueNewInput(&dataFormat,AudioInputCallback, (__bridge void *)(self),
+                       NULL,
+                       kCFRunLoopCommonModes, 0, &queue);*/
 
+    AudioFileCreateWithURL(fileURL, kAudioFileAIFFType, &dataFormat,
+                           kAudioFileFlags_EraseFile, &audioFile);
+    
+   /* UInt32 cookieSize;
+    
+   OSStatus o = AudioQueueGetPropertySize(queue,
+                            kAudioQueueProperty_MagicCookie,
+                             &cookieSize);
+        char* magicCookie = (char*) malloc(cookieSize);
+        o = AudioQueueGetProperty(queue, kAudioQueueProperty_MagicCookie
+                              , magicCookie, &cookieSize) ;
+           o = AudioFileSetProperty(audioFile,kAudioFilePropertyMagicCookieData,cookieSize,magicCookie);
+        free(magicCookie);
+    //}
+    */
+    for(int i = 0; i< NUM_OF_BUFFERS; i++){
+        AudioQueueAllocateBuffer(queue,
+                                 (dataFormat.mSampleRate/10.0f)*dataFormat.mBytesPerFrame,
+                                 &buffers[i]);
+        AudioQueueEnqueueBuffer(queue,buffers[i],0,nil);
+    }
+    //AudioQueueStart(queue,NULL);
+}
+
+
+
+-(void) stopRecording{
+    isRecording = NO;
+    AudioQueueFlush(queue);
+    AudioQueueStop(queue, NO);
+    
+    for(int i = 0;i < NUM_OF_BUFFERS; i++){
+        AudioQueueFreeBuffer(queue, buffers[i]);
+    }
+    
+    AudioQueueDispose(queue,YES);
+    AudioFileClose(audioFile);
+    NSLog(@"Stop Recording!");
 }
 
 -(void) stop{
-    
+
 }
 
+-(void) stop:(NSTimer*)timer{
+    [self stopRecording];
+}
+
+-(void) nop:(int) n {
+    for(int i = 0;i < n;i++){
+        for(int j = 0 ; j < 100; j++){
+            
+        }
+    }
+}
+
+void AudioInputCallback(       void* inUserData,
+                               AudioQueueRef inAQ,
+                               AudioQueueBufferRef inBuffer,
+                               const AudioTimeStamp *inStartTime,
+                               UInt32 inNumberPacketDescriptions,
+                               const AudioStreamPacketDescription *inPacketDescs) {
+    NSLog(@"AudioInputCallback is called!");
+    SoundPicker* recorder = (__bridge SoundPicker*) inUserData;
+    OSStatus status = AudioFileWritePackets(recorder.audioFile, YES, inBuffer->mAudioDataByteSize,
+                                            inPacketDescs, recorder.currentPacket,
+                                            &inNumberPacketDescriptions, inBuffer->mAudioData);
+    
+    if(status == noErr){
+        recorder.currentPacket += inNumberPacketDescriptions;
+        AudioQueueEnqueueBuffer(recorder.queue, inBuffer, 0, nil);
+    }
+}
+
+void AudioInputCallback2(       void* inUserData,
+                        AudioQueueRef inAQ,
+                        AudioQueueBufferRef inBuffer,
+                        const AudioTimeStamp *inStartTime,
+                        UInt32 inNumberPacketDescriptions,
+                        const AudioStreamPacketDescription *inPacketDescs) {
+    NSLog(@"AudioInputCallback is called!");
+}
 
 @end
